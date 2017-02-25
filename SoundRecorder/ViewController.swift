da80c1d32d71e3183aef85e309382d94699e94b9
雷达 <leida@leidadeMacBook-Pro.local>
2017-02-25 14:48:04 +0800

    Initial Commit

---
+++ SoundRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundRecorder
+//
+//  Created by 雷达 on 2017/2/25.
+//  Copyright © 2017年 雷达. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
