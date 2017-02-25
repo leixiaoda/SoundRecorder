9ff8e51d4b79035eda27c9efc647fdd0a895d941
leixiaoda <leixiaoda@163.com>
2017-02-25 21:47:42 +0800

    audioController添加

---
+++ SoundRecorder/Controller/AudioController.swift
@@ -0,0 +1,133 @@
+//
+//  AudioController.swift
+//  SoundRecorder
+//
+//  Created by 雷达 on 2017/2/25.
+//  Copyright © 2017年 雷达. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+final class AudioController: NSObject, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    static let instance = AudioController()
+    private override init() {}
+    static func sharedInstance() -> AudioController {
+        return instance
+    }
+    
+    // test
+//    var counter = 0
+//    func addCounter() {
+//        counter += 1;
+//        print("counter: \(counter)")
+//    }
+    
+    // 录音器
+    var audioRecorder: AVAudioRecorder!
+    // 播放器
+    var audioPlayer: AVAudioPlayer!
+    // 音频会话
+    let audioSession = AVAudioSession.sharedInstance()
+    
+    // 是否有麦克风权限
+    var hasAuthority: Bool = false
+    //定义音频的编码参数[声音采样率, 编码格式, 采集音轨, 音频质量]
+    let recorderSettings = [AVSampleRateKey: NSNumber(value: Float(44100.0)),
+                          AVFormatIDKey: NSNumber(value: Int32(kAudioFormatMPEG4AAC)),
+                          AVNumberOfChannelsKey: NSNumber(value: 1),
+                          AVEncoderAudioQualityKey: NSNumber(value: Int32(AVAudioQuality.medium.rawValue))]
+    
+    // 开始录音的一系列步骤
+    func beginRecording() {
+        // 判断是否有麦克风权限
+        audioSession.requestRecordPermission { (isAllowed) in
+            if !isAllowed {
+                // undone
+//                let alertController = UIAlertController(title: "麦克风权限尚未开启", message: "请移步系统设置->隐私->麦克风，打开本APP的访问权限", preferredStyle: .actionSheet)
+            } else {
+                self.prepareRecording()
+                self.startRecording()
+            }
+        }
+    }
+    
+    // 录音前的准备工作
+    private func prepareRecording() {
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try audioRecorder = AVAudioRecorder(url: newSoundURL(), settings: recorderSettings)
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    // 真正开始录音
+    private func startRecording() {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        
+        if !audioRecorder.isRecording {
+            do {
+                try audioSession.setActive(true)
+                audioRecorder.record()
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+    }
+    
+    // 停止录音
+    func stopRecording() {
+        if audioRecorder.isRecording {
+            audioRecorder.stop()
+            do {
+                try audioSession.setActive(false)
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+    }
+    
+    // 开始播放
+    func beginPlaying(url: URL?) {
+        if !audioRecorder.isRecording {
+            do {
+                if url != nil {
+                    try audioPlayer = AVAudioPlayer(contentsOf: url!)
+                    audioPlayer.delegate = self
+                    audioPlayer.play()
+                } else {
+                    print("url is nil.")
+                }
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+    }
+    
+    // 播放刚录好的音频
+    func beginPlayingTheLatest() {
+        beginPlaying(url: audioRecorder.url)
+    }
+    
+    // 获取新录音文件的地址
+    func newSoundURL() -> URL {
+        let currentDate = Date()
+        let formatter = DateFormatter()
+        // 文件命名规范为：../年月日-时分秒.m4a
+        formatter.dateFormat = "yyyyMMdd-HHmmss"
+        let recordingName = formatter.string(from: currentDate) + ".m4a"
+        
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls[0] as URL
+        let soundURL = documentDirectory.appendingPathComponent(recordingName)
+        return soundURL
+    }
+    
+}
--- SoundRecorder/ViewController.swift
+++ SoundRecorder/ViewController.swift
@@ -13,21 +13,82 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        doInitUI()
+        
         // core data
-        let dataMgr = DataMgr()
-        let model1 = SoundModel(name: "clips1", path: "../../clips1.mp4", duration: 4.688, createTime: Date())
-        let model2 = SoundModel(name: "clips2", path: "../../clips2.mp4", duration: 1.23, createTime: Date())
+//        let dataMgr = DataMgr()
+//        let model1 = SoundModel(name: "clips1", path: "../../clips1.mp4", duration: 4.688, createTime: Date())
+//        let model2 = SoundModel(name: "clips2", path: "../../clips2.mp4", duration: 1.23, createTime: Date())
         
 //        dataMgr.storeSound(model: model1)
 //        dataMgr.storeSound(model: model2)
         
-        dataMgr.deleteAllSound()
+//        dataMgr.deleteAllSound()
+//        
+//        let array = dataMgr.getSound()
+//        if array != nil {
+//            print("count: \(array!.count)")
+//        }
         
-        let array = dataMgr.getSound()
-        if array != nil {
-            print("count: \(array!.count)")
-        }
+        // singleton
+//        AudioController.sharedInstance().addCounter()
+//        AudioController.instance.addCounter()
+        
+        
     }
+    
+    func doInitUI() {
+        // 录音按钮
+        let recordBtn: UIButton = UIButton()
+        let recordBtnSize: CGSize = CGSize(width: 80, height: 30)
+        recordBtn.frame = CGRect.init(x: (self.view.bounds.size.width - recordBtnSize.width) / 2.0, y: self.view.bounds.size.height - 200, width: recordBtnSize.width, height: recordBtnSize.height)
+        recordBtn.setTitle("录音", for: .normal)
+        recordBtn.backgroundColor = UIColor.red
+        recordBtn.layer.borderWidth = 2
+        recordBtn.layer.cornerRadius = 16.0
+        recordBtn.setTitleColor(UIColor.black, for: .normal)
+        recordBtn.addTarget(self, action: #selector(didClickRecordBtn), for: .touchUpInside)
+        
+        view.addSubview(recordBtn)
+        
+        // 结束录音按钮
+        let stopBtn: UIButton = UIButton()
+        let stopBtnSize: CGSize = CGSize(width: 80, height: 30)
+        stopBtn.frame = CGRect.init(x: (self.view.bounds.size.width - stopBtnSize.width) / 2.0, y: self.view.bounds.size.height - 120, width: stopBtnSize.width, height: stopBtnSize.height)
+        stopBtn.setTitle("结束", for: .normal)
+        stopBtn.backgroundColor = UIColor.blue
+        stopBtn.layer.borderWidth = 2
+        stopBtn.layer.cornerRadius = 16.0
+        stopBtn.setTitleColor(UIColor.black, for: .normal)
+        stopBtn.addTarget(self, action: #selector(didClickStopBtn), for: .touchUpInside)
+        
+        view.addSubview(stopBtn)
+        
+        // 播放按钮
+        let playBtn: UIButton = UIButton()
+        let playBtnSize: CGSize = CGSize(width: 80, height: 30)
+        playBtn.frame = CGRect.init(x: self.view.bounds.size.width - 100, y: self.view.bounds.size.height - 120, width: playBtnSize.width, height: playBtnSize.height)
+        playBtn.setTitle("播放", for: .normal)
+        playBtn.backgroundColor = UIColor.green
+        playBtn.layer.borderWidth = 2
+        playBtn.layer.cornerRadius = 16.0
+        playBtn.setTitleColor(UIColor.green, for: .normal)
+        playBtn.addTarget(self, action: #selector(didClickPlayBtn), for: .touchUpInside)
+        
+        view.addSubview(playBtn)
+    }
+    
+    func didClickRecordBtn() {
+        AudioController.sharedInstance().beginRecording()
+    }
+    
+    func didClickStopBtn() {
+        AudioController.sharedInstance().stopRecording()
+    }
+    func didClickPlayBtn() {
+        AudioController.sharedInstance().beginPlayingTheLatest()
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
